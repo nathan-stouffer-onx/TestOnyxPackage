6339919307e2a5aafe8730b1f5453eb191d3a791
nathan-stouffer-onx <nathan.stouffer@onxmaps.com>
2023-10-23 09:13:51 -0600

    attempte to add assets to target list

--- Package.swift
+++ Package.swift
@@ -9,11 +9,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TestOnyxPackage",
-            targets: ["bgfxPackage", "bimgPackage", "bimg_decodePackage", "bxPackage", "coreUtilsPackage", "onyxPackage", "protobufPackage", "shaderPackage", "stylingPackage", "thirdPartyPackage", "webpPackage", "webpdecoderPackage" ]),
+            targets: [ "assets", "bgfxPackage", "bimgPackage", "bimg_decodePackage", "bxPackage", "coreUtilsPackage", "onyxPackage", "protobufPackage", "shaderPackage", "stylingPackage", "thirdPartyPackage", "webpPackage", "webpdecoderPackage" ]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "assets",
+            resources: [
+                .copy("assets/")
+            ]
+        ),
         .binaryTarget(
             name: "bgfxPackage",
             path: "bgfx.xcframework.zip"
